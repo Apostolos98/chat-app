597e7d2f2db99c8a1abcbb59992e22b22a26f452
Apostolos98 <tolisbrasst@gmail.com>
2023-11-23 22:01:57 +0200

    added health route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,9 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
   });
 
+router.get('/health', (req, res) => {
+  res.status(200).send()
+})
+
 
 module.exports = router
